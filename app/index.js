af75dd7a7c7aee8f4dbcf7959b59432c4b20cf7c
Yago Romero <Yago_RomeroTerzis@sluz.ch>
2020-01-17 17:58:08 +0100

    Warenkorb Items hinzugefügt, link zum checkout, details seite verändert

--- app/index.js
+++ app/index.js
@@ -19,6 +19,7 @@ var app = express();
 app.use(express.static(__dirname + '/assets'));
 app.use("/add-products.js", express.static(path.join(__dirname, "./add-products.js")));
 app.use("/add-details.js", express.static(path.join(__dirname, "./add-details.js")));
+app.use("/getTotalPreis.js", express.static(path.join(__dirname, "./getTotalPreis.js")));
 app.use("/data/products.json", express.static(path.join(__dirname, "./data/products.json")));
 app.use("/images", express.static(path.join(__dirname, "./assets/images")));
 app.use("/views", express.static(path.join(__dirname, "./views")));
@@ -40,29 +41,20 @@ app.get("/", function (req, res) {
 app.get("/home", function (req, res) {
     res.redirect('/');
 });
-app.post("/warenkorb", function (req, res) {
-    var productName = req.body.cart;
-    var normalerPreis = req.body.normalPrice;
-    var specialPreis = req.body.specialOffer;
-    if (specialPreis) {
-        res.send(specialPreis);
-    }
-    else if (normalerPreis) {
-        res.send(normalerPreis);
-    }
-});
 app.get("/checkout", function (req, res) {
     sendTemplate("master.html", "checkout.html", {})
         .then(function (rendered) { return res.send(rendered); });
 });
+app.post("/warenkorb", function (req, res) {
+    if (req.body.specialOffer) {
+        res.send("\n        <head>\n        <link rel=\"stylesheet\" href=\"/css/warenkorb-stylesheet.css\">\n        </head>\n        <body>\n            <div>\n                <table>\n                <thead>\n                    <tr>\n                        <th>Product Name</th>\n                        <th>Preis pro Product</th>\n                        <th>Anzahl Producte</th>\n                        <th>Total Preis</th>\n                    </tr>\n                    </thead>\n                    <tbody>\n                    <tr id=\"tableSpecialPreis\">\n                        <td><input type=\"text\" value=\"" + req.body.cart + "\" readonly=\"readonly\"></td>\n                        <td><input id=\"specialPrice\" type=\"number\" value=\"" + req.body.specialOffer + "\" readonly=\"readonly\"></td>\n                        <td><input id=\"countSpecial\" type=\"number\" min=\"0\" max=\"10000\" value=\"0\"></td>\n                        <td id=\"resultat\"></td>\n                    </tr>\n                    </tbody>\n                </table>\n            </div>\n            <div id=\"controls\">\n            <button onClick=\"TotalPreis()\">Total Preis berechnen</button>\n            <button onClick=\"Delete()\">L\u00F6schen</button>\n            <a href=\"/checkout\">Link to Checkout</a>\n            </div>\n            <script>\n                    function TotalPreis(){\n                        var specialPrice = document.getElementById(\"specialPrice\").value;\n                        var countSpecial = document.getElementById(\"countSpecial\").value;\n                        var specialPriceFloat = parseFloat(specialPrice);\n                        var totalprice = specialPriceFloat * countSpecial;\n                        var resultat = document.getElementById(\"resultat\").innerHTML = totalprice;\n                    }\n                </script>\n                <script>\n                function Delete(){\n                    var resultat = document.getElementById(\"resultat\").innerHTML = \" \";\n                    var countSpecial = document.getElementById(\"countSpecial\").value = 0;\n                }\n                </script>\n        <body>\n        ");
+    }
+    else if (req.body.normalPrice) {
+        res.send("\n            <head>\n                <link rel=\"stylesheet\" href=\"/css/warenkorb-stylesheet.css\">\n            </head>\n            <body>\n                <div>\n                    <table>\n                    <thead>\n                        <tr>\n                            <th>Product Name</th>\n                            <th>Preis pro Product</th>\n                            <th>Anzahl Producte</th>\n                            <th>Total Preis</th>\n                        </tr>\n                    </thead>\n                    <tbody>\n                        <tr id=\"tableNormalPreis\">\n                        <td><input id=\"description\" type=\"text\" value=\"" + req.body.cart + "\" readonly=\"readonly\"></td>\n                        <td><input id=\"normalprice\" type=\"number\" value=\"" + req.body.normalPrice + "\" readonly=\"readonly\"></td>\n                        <td><input id=\"countNormal\" type=\"number\" min=\"0\" max=\"10000\" value=\"0\"></td>\n                        <td id=\"resultat\"></td>\n                        </tr>\n                    </tbody>\n                    </table>\n                    <div id=\"controls\">\n                    <button onClick=\"TotalPreis()\">Total Preis berechnen</button>\n                    <button onClick=\"Delete()\">L\u00F6schen</button>\n                    <a href=\"/checkout\">Link to Checkout</a>\n                    </div>\n                    <script>\n                            function TotalPreis(){\n                                var normalPrice = document.getElementById(\"normalprice\").value;\n                                var countNormal = document.getElementById(\"countNormal\").value;\n                                var normalPriceFloat = parseFloat(normalPrice);\n                                var totalprice = normalPriceFloat * countNormal;\n                                var resultat = document.getElementById(\"resultat\").innerHTML = totalprice;\n                            }\n                        </script>\n                        <script>\n                        function Delete(){\n                            var resultat = document.getElementById(\"resultat\").innerHTML = \" \";\n                            var countSpecial = document.getElementById(\"countNormal\").value = 0;\n                        }\n                        </script>\n            <body>\n        ");
+    }
+});
 app.post('/details', function (req, res) {
-    res.send("\n    <head>\n    <link rel=\"stylesheet\" href=\"/css/details-stylesheet.css\">\n    </head>\n    <body>\n    <div>\n        <form action=\"/warenkorb\" method=\"POST\">\n            <input type name=\"cart\" value=\"" + req.body.ProductDetails + "\" readonly=\"readonly\">\n            <img src=/images/" + req.body.ProductFoto + ">\n            <h4>Description: " + req.body.ProductDescription + "</h4>\n            <h4>Specielle Offerte zum Korb hinzuf\u00FCgen: <input name =\"specialOffer\" type=\"submit\" value=" + req.body.ProductSpecialOffer + "></input></h4>\n            <h4>Normaler Preis zum Korb hinzuf\u00FCgen: <input name=\"normalPrice\" type=\"submit\" value=" + req.body.ProductNormalPrice + "></input></h4>      \n        </form>\n    </div>\n    <body>\n    ");
-});
-app.get('/add-products.js', function (req, res) {
-    res.sendFile("./add-products.js");
-});
-app.get('/add-details.js', function (req, res) {
-    res.sendFile("./add-details.js");
+    res.send("\n    <head>\n    <link rel=\"stylesheet\" href=\"/css/details-stylesheet.css\">\n    </head>\n    <body>\n    <div>\n        <form action=\"/warenkorb\" method=\"POST\">\n            <input type name=\"cart\" value=\"" + req.body.ProductDetails + "\" readonly=\"readonly\">\n            <img src=/images/" + req.body.ProductFoto + ">\n            <h4>Description: " + req.body.ProductDescription + "</h4>\n            <h4>Specielle Offerte zum Korb hinzuf\u00FCgen: <input name =\"specialOffer\" type=\"submit\" value=" + req.body.ProductSpecialOffer + ">\n            <input type=\"hidden\" name=\"clicks\" onClick=\"onClick()\">\n            </input></h4>\n            <h4>Normaler Preis zum Korb hinzuf\u00FCgen: <input name=\"normalPrice\" type=\"submit\" value=" + req.body.ProductNormalPrice + "></input></h4>      \n        </form>\n    </div>\n    <body>\n    ");
 });
 var sendTemplate = function (masterPage, contentPage, obj) {
     return new Promise(function (resolve) {
@@ -81,6 +73,6 @@ app.post("/checkout", [
 ], function (req, res) {
     var userFirstName = req.body.userFirstName;
     var userLastName = req.body.userLastName;
-    var userEmail = req.body.userEmai;
+    var userEmail = req.body.userEmail;
 });
 app.listen(8080, function () { console.log("listening"); });
--- app/index.ts
+++ app/index.ts
@@ -15,6 +15,7 @@ const app = express();
 app.use(express.static(__dirname + '/assets'));
 app.use("/add-products.js", express.static(path.join(__dirname, "./add-products.js")));
 app.use("/add-details.js", express.static(path.join(__dirname, "./add-details.js")));
+app.use("/getTotalPreis.js", express.static(path.join(__dirname, "./getTotalPreis.js")));
 app.use("/data/products.json", express.static(path.join(__dirname, "./data/products.json")));
 app.use("/images", express.static(path.join(__dirname, "./assets/images")));
 app.use("/views", express.static(path.join(__dirname, "./views")));
@@ -42,24 +43,112 @@ app.get("/home", (req, res) => {
    res.redirect('/');
 });
 
-app.post("/warenkorb", (req, res) => {
-    var productName = req.body.cart;
-    var normalerPreis = req.body.normalPrice;
-    var specialPreis = req.body.specialOffer;
-
-    if(specialPreis){
-        res.send( specialPreis);
-    } else if(normalerPreis){
-        res.send(normalerPreis);
-    }
-});
-
-
 app.get("/checkout", (req,res) => {
     sendTemplate("master.html", "checkout.html", {})
         .then(rendered => res.send(rendered));
 });
 
+
+
+app.post("/warenkorb", (req, res) => {
+    if(req.body.specialOffer){
+        res.send(`
+        <head>
+        <link rel="stylesheet" href="/css/warenkorb-stylesheet.css">
+        </head>
+        <body>
+            <div>
+                <table>
+                <thead>
+                    <tr>
+                        <th>Product Name</th>
+                        <th>Preis pro Product</th>
+                        <th>Anzahl Producte</th>
+                        <th>Total Preis</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr id="tableSpecialPreis">
+                        <td><input type="text" value="${req.body.cart}" readonly="readonly"></td>
+                        <td><input id="specialPrice" type="number" value="${req.body.specialOffer}" readonly="readonly"></td>
+                        <td><input id="countSpecial" type="number" min="0" max="10000" value="0"></td>
+                        <td id="resultat"></td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+            <div id="controls">
+            <button onClick="TotalPreis()">Total Preis berechnen</button>
+            <button onClick="Delete()">Löschen</button>
+            <a href="/checkout">Link to Checkout</a>
+            </div>
+            <script>
+                    function TotalPreis(){
+                        var specialPrice = document.getElementById("specialPrice").value;
+                        var countSpecial = document.getElementById("countSpecial").value;
+                        var specialPriceFloat = parseFloat(specialPrice);
+                        var totalprice = specialPriceFloat * countSpecial;
+                        var resultat = document.getElementById("resultat").innerHTML = totalprice;
+                    }
+                </script>
+                <script>
+                function Delete(){
+                    var resultat = document.getElementById("resultat").innerHTML = " ";
+                    var countSpecial = document.getElementById("countSpecial").value = 0;
+                }
+                </script>
+        <body>
+        `);
+
+    } else if(req.body.normalPrice){
+        res.send(`
+            <head>
+                <link rel="stylesheet" href="/css/warenkorb-stylesheet.css">
+            </head>
+            <body>
+                <div>
+                    <table>
+                    <thead>
+                        <tr>
+                            <th>Product Name</th>
+                            <th>Preis pro Product</th>
+                            <th>Anzahl Producte</th>
+                            <th>Total Preis</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr id="tableNormalPreis">
+                        <td><input id="description" type="text" value="${req.body.cart}" readonly="readonly"></td>
+                        <td><input id="normalprice" type="number" value="${req.body.normalPrice}" readonly="readonly"></td>
+                        <td><input id="countNormal" type="number" min="0" max="10000" value="0"></td>
+                        <td id="resultat"></td>
+                        </tr>
+                    </tbody>
+                    </table>
+                    <div id="controls">
+                    <button onClick="TotalPreis()">Total Preis berechnen</button>
+                    <button onClick="Delete()">Löschen</button>
+                    <a href="/checkout">Link to Checkout</a>
+                    </div>
+                    <script>
+                            function TotalPreis(){
+                                var normalPrice = document.getElementById("normalprice").value;
+                                var countNormal = document.getElementById("countNormal").value;
+                                var normalPriceFloat = parseFloat(normalPrice);
+                                var totalprice = normalPriceFloat * countNormal;
+                                var resultat = document.getElementById("resultat").innerHTML = totalprice;
+                            }
+                        </script>
+                        <script>
+                        function Delete(){
+                            var resultat = document.getElementById("resultat").innerHTML = " ";
+                            var countSpecial = document.getElementById("countNormal").value = 0;
+                        }
+                        </script>
+            <body>
+        `);
+    }
+});
 app.post('/details',(req,res) =>{
     res.send(`
     <head>
@@ -71,7 +160,9 @@ app.post('/details',(req,res) =>{
             <input type name="cart" value="${req.body.ProductDetails}" readonly="readonly">
             <img src=/images/${req.body.ProductFoto}>
             <h4>Description: ${req.body.ProductDescription}</h4>
-            <h4>Specielle Offerte zum Korb hinzufügen: <input name ="specialOffer" type="submit" value=${req.body.ProductSpecialOffer}></input></h4>
+            <h4>Specielle Offerte zum Korb hinzufügen: <input name ="specialOffer" type="submit" value=${req.body.ProductSpecialOffer}>
+            <input type="hidden" name="clicks" onClick="onClick()">
+            </input></h4>
             <h4>Normaler Preis zum Korb hinzufügen: <input name="normalPrice" type="submit" value=${req.body.ProductNormalPrice}></input></h4>      
         </form>
     </div>
@@ -79,16 +170,6 @@ app.post('/details',(req,res) =>{
     `
     )
 });
-
-app.get('/add-products.js', (req,res) =>{
-    res.sendFile("./add-products.js");
-})
-
-app.get('/add-details.js', (req,res) =>{
-    res.sendFile("./add-details.js");
-})
-
-
 
 const sendTemplate = (masterPage: string, contentPage: string, obj: any) => {
     return new Promise((resolve) => {
@@ -100,16 +181,15 @@ const sendTemplate = (masterPage: string, contentPage: string, obj: any) => {
     });    
 }
 
-
 //form validierung
 app.post("/checkout", [
     check('userFirstName').not().isEmpty(),
     check('userLastName').not().isEmpty(),
     check('email').isEmail()
-  ], (req: { body: { userFirstName: any; userLastName: any; userEmai: any; }; }, res: any) => {
+  ], (req: { body: { userFirstName: any; userLastName: any; userEmail: any; }; }, res: any) => {
     const userFirstName  = req.body.userFirstName
     const userLastName   = req.body.userLastName
-    const userEmail = req.body.userEmai
+    const userEmail = req.body.userEmail
   });
 
 app.listen(8080, () => {console.log("listening");});
